9c53e5888bcf4261aa58ec661c687caeed470408
notdaviarantes <daviarantes13@gmail.com>
2023-09-05 15:27:51 -0300

    initial file setup

---
+++ src/app/controllers/ContactController.js
@@ -0,0 +1,23 @@
+class ContactController {
+  index(request, response) {
+    response.send('send from contact controller');
+  }
+
+  show() {
+
+  }
+
+  store() {
+
+  }
+
+  update() {
+
+  }
+
+  delete() {
+
+  }
+}
+
+module.exports = new ContactController();
