e3b6b8575cc303b1bd45ccbf9e24f2d57f81ba82
notdaviarantes <daviarantes13@gmail.com>
2023-09-12 14:11:38 -0300

    creating categories module

---
+++ src/app/controllers/CategoryController.js
@@ -0,0 +1,28 @@
+const CategoriesRepository = require('../repositories/CategoriesRepository');
+
+class CategoryController {
+  async index(request, response) {
+    const categories = await CategoriesRepository.findAll();
+    response.json(categories);
+  }
+
+  async store(request, response) {
+    const { name } = request.body;
+
+    if (!name) {
+      return response.status(400).json({ error: 'name must be provided' });
+    }
+
+    const categoryExists = await CategoriesRepository.findByName(name);
+
+    if (categoryExists) {
+      return response.status(400).json({ error: 'this category already exists' });
+    }
+
+    const category = await CategoriesRepository.create({ name });
+
+    response.json(category);
+  }
+}
+
+module.exports = new CategoryController();
---
+++ src/app/repositories/CategoriesRepository.js
@@ -0,0 +1,20 @@
+const db = require('../../database/index');
+
+class CategoriesRepository {
+  async findAll() {
+    const row = await db.query('SELECT * FROM categories ORDER BY name');
+    return row;
+  }
+
+  async findByName(name) {
+    const [row] = await db.query('SELECT * FROM categories WHERE name = $1', [name]);
+    return row;
+  }
+
+  async create({ name }) {
+    const [row] = await db.query('INSERT INTO categories(name) VALUES($1) RETURNING *', [name]);
+    return row;
+  }
+}
+
+module.exports = new CategoriesRepository();
