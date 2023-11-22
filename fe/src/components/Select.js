b3ce97770103804a105930b65cb962c12eb03cd8
notdaviarantes <daviarantes13@gmail.com>
2023-11-22 10:28:36 -0300

    feat: add Input and Select components

---
+++ fe/src/components/Select.js
@@ -0,0 +1,18 @@
+import styled from 'styled-components';
+
+export default styled.select`
+  width: 100%;
+  background: #ffffff;
+  height: 52px;
+  border-radius: 4px;
+  box-shadow: 0px 4px 10px rgba(0, 0, 0, 0.04);
+  border: 2px solid #ffffff;
+  outline: none;
+  padding: 0 16px;
+  font-size: 16px;
+  transition: border-color 0.2s ease-in;
+
+  &:focus {
+    border-color: ${({ theme }) => theme.colors.primary.main}
+  }
+`;
--- fe/src/pages/NewContact/index.js
+++ fe/src/pages/NewContact/index.js
@@ -1,7 +1,17 @@
 import PageHeader from '../../components/PageHeader';
+import Input from '../../components/Input';
+import Select from '../../components/Select';
 
 export default function NewContact() {
   return (
-    <PageHeader title="Criar novo contato" />
+    <>
+      <PageHeader title="Criar novo contato" />
+      <Input type="text" placeholder="Nome" />
+
+      <Select>
+        <option value="123">Instagram</option>
+        <option value="456">Twitter</option>
+      </Select>
+    </>
   );
 }
