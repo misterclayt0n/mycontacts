62e27f6c86cb5d82e99dc6ae558281477591dfa4
notdaviarantes <daviarantes13@gmail.com>
2023-11-27 13:25:46 -0300

    feat: danger status on Modal component

--- fe/src/pages/Home/index.js
+++ fe/src/pages/Home/index.js
@@ -11,7 +11,7 @@ import Modal from '../../components/Modal';
 export default function Home() {
   return (
     <Container>
-      <Modal />
+      <Modal danger />
 
       <InputSearchContainer>
         <input type="text" placeholder="Pesquisar contato..." />
