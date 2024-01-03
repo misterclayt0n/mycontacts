f726cd8c824fe5f25e0ee5daac9117772acf9eaf
notdaviarantes <daviarantes13@gmail.com>
2024-01-03 00:22:32 -0300

    feat: add contact creation

--- fe/src/pages/NewContact/index.js
+++ fe/src/pages/NewContact/index.js
@@ -1,12 +1,29 @@
 import PageHeader from '../../components/PageHeader';
 import ContactForm from '../../components/ContactForm';
+import ContactsService from '../../services/ContactsService';
 
 export default function NewContact() {
+  async function handleSubmit(formData) {
+    try {
+      const contact = {
+        name: formData.name,
+        email: formData.email,
+        phone: formData.phone,
+        category_id: formData.categoryId,
+      };
+
+      const response = await ContactsService.createContact(contact);
+      console.log(response);
+    } catch {
+      alert('Ocorreu um erro ao cadastrar um contato');
+    }
+  }
+
   return (
     <>
-      <PageHeader title="Criar novo contato" />
+      <PageHeader title="Novo contato" />
 
-      <ContactForm buttonLabel="Cadastrar" />
+      <ContactForm buttonLabel="Cadastrar" onSubmit={handleSubmit} />
     </>
   );
 }
